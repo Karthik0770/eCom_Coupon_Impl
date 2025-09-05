38d29dc66d350b0ceafbf7fa6bf8a2d57612178d
Karthik.Iyer <Karthik.Iyer@ril.com>
2025-09-05 10:53:01 +0530

    Changed method of getProducts to GET from POST

--- src/main/java/com/monk/backend/controller/ProductController.java
+++ src/main/java/com/monk/backend/controller/ProductController.java
@@ -7,10 +7,7 @@ import com.monk.backend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,7 +36,7 @@ public class ProductController {
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
-    @PostMapping("get")
+    @GetMapping("get")
     public ResponseEntity<ResponseDto<List<Product>>> fetchProducts(){
         ResponseDto<List<Product>> response = new ResponseDto<>();
 
